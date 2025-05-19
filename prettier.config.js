806ac4a74b9361e485c25afd4e58337df1d91796
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-19 18:29:42 +0100

    Prettier support

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "none",
+    printWidth: 100
+};
+
+export default config;
+
+
