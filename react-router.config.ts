961ef0ea7ff07a00e6c7bca28f3b84681722fafe
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-25 17:00:41 +0100

    support defining base public path

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,6 @@ export default {
     // Config options...
     // Server-side render by default, to enable SPA mode set this to `false`
     appDirectory: "src",
+    basename: process.env.NODE_ENV === "production" ? process.env.BASE_PUBLIC_PATH : "/",
     ssr: true
 } satisfies Config;
