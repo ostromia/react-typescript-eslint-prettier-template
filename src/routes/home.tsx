9d211b776a23bd0b7a151c00bb9de13359126ed6
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-25 16:48:47 +0100

    replace React Router boilerplate with modified React boilerplate

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,10 @@
-import { Welcome } from "../welcome/welcome";
+import { useState } from "react";
+
+import reactRouterLogo from "../assets/react-router.svg";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../assets/vite.svg";
 import type { Route } from "./+types/home";
+import "./home.css";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -8,6 +13,37 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-export default function Home() {
-    return <Welcome />;
+function Home() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo vite" alt="Vite logo" />
+                </a>
+
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react logo-spin" alt="React logo" />
+                </a>
+
+                <a href="https://reactrouter.com/" target="_blank">
+                    <img src={reactRouterLogo} className="logo react-router" alt="Vite logo" />
+                </a>
+            </div>
+
+            <div>
+                <h1>Vite + React + React Router</h1>
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to test HMR
+                    </p>
+                </div>
+                <p className="read-the-docs">Click on the logos to learn more</p>
+            </div>
+        </div>
+    );
 }
+
+export default Home;
