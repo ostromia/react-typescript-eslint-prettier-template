fcff08e6ed6b1a90ac3b5ee9e504f71e41f24026
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-25 16:57:43 +0100

    remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [reactRouter(), tsconfigPaths()]
+    plugins: [reactRouter()]
 });
