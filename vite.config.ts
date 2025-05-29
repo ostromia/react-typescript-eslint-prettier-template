7ee733cdf5b14936d1a06c866c67c44088646f8e
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-29 16:26:24 +0100

    clean Vite scaffold with React framework and TypeScript variant

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === "production" ? `/${process.env.BASE_PUBLIC_PATH}/` : "/",
-    plugins: [reactRouter()]
+    plugins: [react()]
 });
